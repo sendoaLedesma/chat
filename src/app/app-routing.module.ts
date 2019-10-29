b2b40368e0d816fc92330c2dffa93a1938c812ba
Angular Usuario <angular@ipartekaula.com>
2019-10-29 18:29:32 +0100

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SalaComponent } from './sala/sala.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/chat/login', pathMatch: 'full'},
+  {path: 'chat/login', component: LoginComponent },
+  {path: 'chat/salas', component: SalaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
